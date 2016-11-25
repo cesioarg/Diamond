0628aae803ddb677f4889b74dfd6d0b1c40d3986
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2016-11-25 05:12:16 -0700

    Set a proper email address in setup.py
    This should fix issues uploading the package to twine, as per https://github.com/h5py/h5py/commit/0efaceeffb3693a4dd1ef0cd60b93d74c870cee0

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ setup(
     version=version,
     url='https://github.com/python-diamond/Diamond',
     author='The Diamond Team',
-    author_email='https://github.com/python-diamond/Diamond',
+    author_email='pypi@josediazgonzalez.com',
     license='MIT License',
     description='Smart data producer for graphite graphing package',
     package_dir={'': 'src'},
